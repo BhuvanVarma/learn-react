7647ea974c881371ad9671b5e98b501c70619a5f
Bhuvan Varma Konduru <bhuvan.varma123@gmail.com>
2024-06-02 15:42:10 +0530

    added react files

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Food from "./components/Food"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
+function App() {
+  return(
+    <>
+      <Header/>
+      <Food/>
+      <Footer/>
+    </>
+    
+  )
+}
+
+export default App
